6874f6b59b5dd323cf0bad7076998a3068c5708f
lawliet <jian.wu@mobilemd.cn>
2018-12-15 17:24:37 +0800

    fix: modify Same Tree null

--- SameTree.js
+++ SameTree.js
@@ -22,9 +22,7 @@
 const isSameTree = function(p, q) {
   if (p === q) {
     return true;
-  } else if (p === null || q === null) {
-    return false;
-  } else if (p.val === q.val) {
+  } else if (p !== null && q !== null && p.val === q.val) {
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
   } else {
     return false;
